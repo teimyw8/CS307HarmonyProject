97da0ed9d782bb553cb7a4af4e35e99470c55a98
Michael Con <michaelcon@Michaels-MacBook-Pro.local>
2022-10-02 23:14:33 -0400

    Created Branch and made initial files needed for friends_list_screen.

---
+++ lib/screens/friends_list_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class friends_list_page extends StatefulWidget {
+  const friends_list_page({Key? key}) : super(key: key);
+
+  @override
+  _friends_list_pageState createState() => _friends_list_pageState();
+}
+
+class _friends_list_pageState extends State<friends_list_page> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
