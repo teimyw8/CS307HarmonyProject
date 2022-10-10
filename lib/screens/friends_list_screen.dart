0fa91e249ab860848bc678576e37cc1a1a0d2ee7
Michael Con <michaelcon@Michaels-MacBook-Pro.local>
2022-10-09 23:32:56 -0400

    Completed UI and queries that show the friends on the list. Also added the buttons that will eventually lead fo chats with the respective friends and the future button for deleting the friend from your list.

--- lib/screens/friends_list_screen.dart
+++ lib/screens/friends_list_screen.dart
@@ -1,5 +1,4 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class friends_list_page extends StatefulWidget {
@@ -15,20 +14,21 @@ class _friends_list_pageState extends State<friends_list_page> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Friends List'),
-        actions: [IconButton(onPressed: () {
-          debugPrint('refresh button - friends list');
-          setState(() {}); //refresh this page to show any changes
-        }, icon: Icon(Icons.refresh))],
+        actions: [
+          IconButton(
+              onPressed: () {
+                debugPrint('refresh button - friends list');
+                setState(() {}); //refresh this page to show any changes
+              },
+              icon: Icon(Icons.refresh))
+        ],
       ),
       body: Column(
-        children: <Widget>[
-            friendsListView()
-        ],
+        children: <Widget>[friendsListView()],
       ),
     );
   }
 }
-
 
 class friendsListView extends StatefulWidget {
   const friendsListView({Key? key}) : super(key: key);
@@ -40,8 +40,44 @@ class friendsListView extends StatefulWidget {
 class _friendsListViewState extends State<friendsListView> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('hello world'),
+    return StreamBuilder<QuerySnapshot>(
+      stream: FirebaseFirestore.instance.collection('users').snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return const Text('Error, reload');
+        }
+
+        final List<DocumentSnapshot> documents = snapshot.data!.docs;
+        return ListView(
+            shrinkWrap: true,
+            children: documents
+                .map((e) => Card(
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        children: <Widget>[
+                          Text(e['firstName']),
+                          Spacer(),
+                          Container(
+                            child: Row(
+                              children: [
+                                IconButton(
+                                  icon: const Icon(Icons.message),
+                                  color: Colors.green,
+                                  onPressed: () {},
+                                ),
+                                IconButton(
+                                  icon: const Icon(Icons.remove_circle_outline),
+                                  color: Colors.red,
+                                  onPressed: () {},
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
+                      ),
+                    ))
+                .toList());
+      },
     );
   }
 }
