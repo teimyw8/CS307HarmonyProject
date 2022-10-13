cdb6fd217859278b24aa0354d1b3146d38749916
Alex Bakshi Richardson <hidden>
2022-10-12 21:29:34 -0400

    Created functions to check that username/password meet requirements

--- lib/providers/edit_profile_provider.dart
+++ lib/providers/edit_profile_provider.dart
@@ -5,12 +5,14 @@ import 'package:provider/provider.dart';
 
 import '../helpers/text_styles.dart';
 import '../models/user_model.dart';
+import '../services/auth_service.dart';
 import '../services/firestore_service.dart';
 import 'package:get/get.dart';
 
 class EditProfileProvider with ChangeNotifier {
   UserModel? currentUserModel;
   bool isEditing = false;
+  AuthService get _authService => GetIt.instance<AuthService>();
   FirestoreService get _firestoreService => GetIt.instance<FirestoreService>();
   final AuthProvider _authProvider =
       Provider.of<AuthProvider>(Get.context!, listen: false);
@@ -27,6 +29,46 @@ class EditProfileProvider with ChangeNotifier {
       isEditing = true;
     }
     print(isEditing);
+  }
+
+  ///this function modifies user profile data in database
+  Future<void> setUserInfo(String uid, String json) async {
+
+    notifyListeners();
+  }
+
+  bool meetsUsernameReqs(String username) {
+    if (username.length < 5 || username.length > 15) {
+      return false;
+    }
+
+    for (int i = 0; i < username.length; i++) {
+      String c = username.substring(i, i + 1);
+      if (!c.isNum && !c.isAlphabetOnly && c.compareTo(".") != 0) {
+        return false;
+      }
+    }
+
+    //TODO: check to see if username already exists
+    return true;
+  }
+
+  bool meetsPasswordReqs(String password) {
+    if (password.length < 8 || password.length > 26) {
+      return false;
+    }
+
+    bool containsNum = false;
+    bool containsSpecial = false;
+    for (int i = 0; i < password.length; i++) {
+      String c = password.substring(i, i + 1);
+      if (c.isNum) {
+        containsNum = true;
+      } else if (!c.isAlphabetOnly) {
+        containsSpecial = true;
+      }
+    }
+    return containsNum && containsSpecial;
   }
 
   String getUserEmail() {
