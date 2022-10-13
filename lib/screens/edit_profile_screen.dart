0b73a514ade2ab61fc36ec9f6d0a845c11e32a89
Alex Bakshi Richardson <hidden>
2022-10-13 00:39:04 -0400

    Added error popup and changed made edit button swap to save symbol when in editing mode

--- lib/screens/edit_profile_screen.dart
+++ lib/screens/edit_profile_screen.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:get_it/get_it.dart';
 import 'package:harmony_app/providers/edit_profile_provider.dart';
 import 'package:harmony_app/widgets/common_widgets/custom_app_bar.dart';
+import 'package:harmony_app/widgets/common_widgets/pop_up_dialog.dart';
 import 'package:provider/provider.dart';
 
 import '../helpers/colors.dart';
 import '../models/user_model.dart';
-import '../services/firestore_service.dart';
 import 'friends_list_screen.dart';
 
 class EditProfileScreen extends StatefulWidget {
@@ -22,6 +21,10 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
       Provider.of<EditProfileProvider>(Get.context!, listen: false);
   final formKey = GlobalKey<FormState>();
   UserModel? temp;
+
+  _test() {
+    print("");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,87 +54,91 @@ class _EditProfileScreenState extends State<EditProfileScreen> {
               if (!_editProfileProvider.isEditing) {
                 temp = _editProfileProvider.currentUserModel;
                 formKey.currentState!.save();
-                _editProfileProvider.setUserInfo(
-                    UserModel.fromJson(temp!.toJson()));
+                _editProfileProvider
+                    .setUserInfo(UserModel.fromJson(temp!.toJson()));
               }
               setState(() {});
             },
-            child: const Icon(Icons.edit),
+            child: (_editProfileProvider.isEditing) ? const Icon(Icons.save) : const Icon(Icons.edit),
           ),
-          body: Stack(
-            children: [
-              Form(
-          key: formKey,
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 8, vertical: 16),
-                        child: TextFormField(
-                          decoration: const InputDecoration(
-                            border: UnderlineInputBorder(),
-                            labelText: 'Username',
-                          ),
-                          initialValue: _editProfileProvider.getUserEmail(),
-                          onSaved: (String? value) {},
-                          enabled: _editProfileProvider.isEditing,
+          body: SingleChildScrollView(
+            child: Form(
+              key: formKey,
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 8, vertical: 16),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: 'Username',
                         ),
+                        initialValue: _editProfileProvider.getUserEmail(),
+                        onSaved: (String? value) {},
+                        enabled: _editProfileProvider.isEditing,
                       ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 8, vertical: 16),
-                        child: TextFormField(
-                          decoration: const InputDecoration(
-                            border: UnderlineInputBorder(),
-                            labelText: 'First Name',
-                          ),
-                          initialValue: _editProfileProvider.getUserFirst(),
-                          onSaved: (String? value) {
-                            value ??= "";
-                            temp!.firstName = value;
-                          },
-                          enabled: _editProfileProvider.isEditing,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 8, vertical: 16),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: 'First Name',
                         ),
+                        initialValue: _editProfileProvider.getUserFirst(),
+                        onSaved: (String? value) {
+                          value ??= "";
+                          temp!.firstName = value;
+                        },
+                        enabled: _editProfileProvider.isEditing,
                       ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 8, vertical: 16),
-                        child: TextFormField(
-                          decoration: const InputDecoration(
-                            border: UnderlineInputBorder(),
-                            labelText: 'Last Name',
-                          ),
-                          initialValue: _editProfileProvider.getUserLast(),
-                          onSaved: (String? value) {
-                            value ??= "";
-                            temp!.lastName = value;
-
-                          },
-                          enabled: _editProfileProvider.isEditing,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 8, vertical: 16),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: 'Last Name',
                         ),
+                        initialValue: _editProfileProvider.getUserLast(),
+                        onSaved: (String? value) {
+                          value ??= "";
+                          temp!.lastName = value;
+                        },
+                        enabled: _editProfileProvider.isEditing,
                       ),
-                      Padding(
-                        padding: const EdgeInsets.symmetric(
-                            horizontal: 8, vertical: 16),
-                        child: TextFormField(
-                          decoration: const InputDecoration(
-                            border: UnderlineInputBorder(),
-                            labelText: 'Username',
-                          ),
-                          initialValue: _editProfileProvider.getUserName(),
-                          onSaved: (String? value) {
-                            value ??= "";
-                            if (_editProfileProvider.meetsUsernameReqs(value)) {
-                              temp!.userName = value;
-                            }
-                          },
-                          enabled: _editProfileProvider.isEditing,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 8, vertical: 16),
+                      child: TextFormField(
+                        decoration: const InputDecoration(
+                          border: UnderlineInputBorder(),
+                          labelText: 'Username',
                         ),
+                        initialValue: _editProfileProvider.getUserName(),
+                        onSaved: (String? value) {
+                          value ??= "";
+                          if (_editProfileProvider.meetsUsernameReqs(value)) {
+                            temp!.userName = value;
+                          } else {
+                            PopUpDialog.showErrorPopUpDialog(
+                                title: 'Username incorrect',
+                                message:
+                                    'Username must be between 5 and 15 characters (inclusive), only contain alphanumeric characters or ".", and be a unique username.',
+                                confirmLabel: 'Close',
+                                onCloseClick: _test);
+                          }
+                        },
+                        enabled: _editProfileProvider.isEditing,
                       ),
-                    ]),
-              ),
-            ],
+                    ),
+                  ]),
+            ),
           ),
         ),
       ]),
