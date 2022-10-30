1aa7c3c37cec1e800b2299fa64c4f38f88f12bc7
alexnat39 <natalenko2001@gmail.com>
2022-10-30 18:51:37 -0400

    set up a chat_service.dart file for functions used for chat communications

--- lib/models/chat_model.dart
+++ lib/models/chat_model.dart
@@ -3,14 +3,24 @@ import 'package:harmony_app/models/message_model.dart';
 class ChatModel {
   List<MessageModel> messages;
   String partnerUserId;
+  String chatId;
 
-  ChatModel({required this.messages, required this.partnerUserId});
+  ChatModel(
+      {required this.messages,
+      required this.partnerUserId,
+      required this.chatId});
 
   factory ChatModel.fromJson(Map<String, dynamic> json) => ChatModel(
       messages: json["messages"] == null
           ? []
           : List<MessageModel>.from(
-          json["messages"].map((x) => MessageModel.fromJson(x))),
-      partnerUserId: json['partnerUserId'] ?? ""
-  );
+              json["messages"].map((x) => MessageModel.fromJson(x))),
+      partnerUserId: json['partnerUserId'] ?? "",
+      chatId: json['chatId'] ?? '');
+
+  Map<String, dynamic> toJson() => {
+    "messages": List<dynamic>.from(messages.map((x) => x.toJson())),
+    "partnerUserId": partnerUserId,
+    "chatId": chatId,
+  };
 }
---
+++ lib/services/chat_service.dart
@@ -0,0 +1,31 @@
+class ChatService {
+  ///this function creates a new chat in Firebase Firestore
+  Future<void> createNewChatInFirebaseFirestore({required String partnerId}) async {
+    try {
+
+    } catch (e) {
+
+    }
+  }
+
+
+  ///this function receives chat updates from Firebase Firestore
+  Future<void> receiveUpdatesFromChat({required String chatId}) async {
+    try {
+
+    } catch (e) {
+
+    }
+  }
+
+
+  ///this function sends a chat message to Firebase Firestore chat
+  Future<void> sendMessageToChat({required String chatId, required String messageContent}) async {
+    try {
+
+    } catch (e) {
+
+    }
+  }
+
+}
