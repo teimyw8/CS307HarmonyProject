e0be392e3e4b4cd2895a5192591285fd80bb2b0f
prathamagrawal <agraw144@purdue.edu>
2022-10-30 20:17:03 -0400

    Attempted to upload image to firebase storage

--- lib/screens/profile_picture.dart
+++ lib/screens/profile_picture.dart
@@ -54,7 +54,7 @@ class _HomeState extends State<Home> {
                     onPressed: () {
                       Navigator.pop(context);
                       getImage(ImageSource.gallery);
-                    },
+                    },s
                     child: Row(
                       children: [
                         Icon(Icons.image),
